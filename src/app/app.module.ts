6dce1580a8f27963ef8f3b3bb1c3e137abb0c45b
Sergio Guachalla <sergioguachalla0@gmail.com>
2022-10-30 18:31:54 -0400

    Routeamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'lista-productos', component: ListaProductosComponent },
+      { path: 'contactos', component: ContactosComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
