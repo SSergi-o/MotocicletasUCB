f3f5292668f84b29e0a4b132dd112e76667402b2
Sergio Guachalla <sergioguachalla0@gmail.com>
2022-10-31 18:40:47 -0400

    Home1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
+import { MatCardModule} from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { ContactosComponent } from './components/contactos/contactos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,13 +19,15 @@ import { ContactosComponent } from './components/contactos/contactos.component';
     ProductosComponent
   ],
   imports: [
+    MatCardModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path : '', component : HomeComponent},
       { path: 'productos', component: ProductosComponent },
       { path: 'contactos', component: ContactosComponent },
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
